100a6088c1aceae3f11d60316df1b75438526c78
Tiffani <tiffani.grace.elmore@gmail.com>
2018-11-06 20:24:30 -0600

    new build

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Book = require('../models/Book');
+const Book = require('../models/book');
 const session = require('cookie-session');
 const config = require('../config');
 
